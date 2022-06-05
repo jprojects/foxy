678189e7e9312be099674104f0aff7dbec30da57
Carles Serra <carles@afi.cat>
2022-06-05 09:41:01 +0200

    Classe Tools: nou membre UTF8ToW1252()

--- classes/tools.php
+++ classes/tools.php
@@ -20,5 +20,9 @@ class Tools
 		$ending_date = str_replace('/' , '-' , $ending_date);
 		return $field." BETWEEN '".date('Y-m-d', strtotime($starting_date))." 00:00:00' AND '".date('Y-m-d', strtotime($ending_date))." 23:59:59'";
 	}
+	
+	public function UTF8ToW1252($cadena) {
+		return iconv('UTF-8', 'windows-1252', '$cadena);
+	}
 }
 ?>
